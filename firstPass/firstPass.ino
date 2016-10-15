fa4c8f3df87c2b93c93d05d369c3dd0c84cd1b2a
cmoser96 <cm898946@gmail.com>
2016-10-15 00:24:22 -0400

    Printing the values to plot

--- firstPass/firstPass.ino
+++ firstPass/firstPass.ino
@@ -35,22 +35,22 @@ void loop() {
     // If the left sensor hits the tape, turn off the right motor
     m1->setSpeed(mainSpeed);
     m2->setSpeed(mainSpeed - diffSpeed);
-    Serial.print(mainSpeed);
-    Serial.print(mainSpeed - diffSpeed);
+    Serial.println(mainSpeed);
+    Serial.println(mainSpeed - diffSpeed);
   }
   else if(rightValue > irThreshhold){
     // If the right sensor hits the tape, turn off the left motor
     m1->setSpeed(mainSpeed - diffSpeed);
     m2->setSpeed(mainSpeed);
-    Serial.print(mainSpeed - diffSpeed);
-    Serial.print(mainSpeed);
+    Serial.println(mainSpeed - diffSpeed);
+    Serial.println(mainSpeed);
   }
   else{
     // Charge forward!
     m1->setSpeed(mainSpeed);
     m2->setSpeed(mainSpeed);
-    Serial.print(mainSpeed);
-    Serial.print(mainSpeed);
+    Serial.println(mainSpeed);
+    Serial.println(mainSpeed);
   }
   
 }
@@ -76,7 +76,7 @@ void irSensorRead()
   leftValue = leftValue/5;
   rightValue = rightValue/5;
 
-  Serial.print(leftValue);
+  Serial.println(leftValue);
   Serial.println(rightValue);
 }
 
