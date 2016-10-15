d23e77b4b203ff790b75987e8abe3d18083f6c19
cmoser96 <cm898946@gmail.com>
2016-10-14 20:33:26 -0400

    bang bang should be working

--- firstPass/firstPass.ino
+++ firstPass/firstPass.ino
@@ -4,66 +4,76 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
-#define sensor1 A0 // Left IR sensor
-#define sensor2 A1 // Right IR sensor
+#define leftSensor A5 // Left IR sensor
+#define rightSensor A4 // Right IR sensor
 
 byte input[256];
-int irVal1;
-int irVal2;
-int ir_threshold = 950;
+int leftValue;
+int rightValue;
 
 Adafruit_MotorShield ms = Adafruit_MotorShield();
-Adafruit_DCMotor *m1 = ms.getMotor(1);
-Adafruit_DCMotor *m2 = ms.getMotor(2);
-int m1_speed = 100;
-int m2_speed = 100;
+Adafruit_DCMotor *m1 = ms.getMotor(1); // leftMotor
+Adafruit_DCMotor *m2 = ms.getMotor(2); // rightMotor
+volatile int mainSpeed = 100;
+volatile int diffSpeed = 30;
+volatile int irThreshhold = 950;
 
 void setup() {
   // put your setup code here, to run once:
   ms.begin();
-  m1->setSpeed(m1_speed);
-  m2->setSpeed(m2_speed);
-  Serial.begin(19200);
+  m1->setSpeed(mainSpeed);
+  m2->setSpeed(mainSpeed);
+  m1->run(FORWARD);
+  m2->run(FORWARD);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
   // read irSensor valye
   irSensorRead();
 
-  /*
-   * FORWARD, BACKWARD, RELEASE
-   * m1->run(FORWARD);
-   * m2->run(FORWARD);
-   */
-  
-  // print sensor vals
-  Serial.println(irVal1);
-  Serial.println(irVal2);
+  if(leftValue < irThreshhold){
+    m1->setSpeed(mainSpeed);
+    m2->setSpeed(mainSpeed - diffSpeed);
+  }
+  else if(rightValue > irThreshhold){
+    m1->setSpeed(mainSpeed - diffSpeed);
+    m2->setSpeed(mainSpeed);
+  }
+  else{
+    m1->setSpeed(mainSpeed);
+    m2->setSpeed(mainSpeed);
+  }
 }
 
 void irSensorRead()
 {
   // irSensorRead reads the sensor 5 times and returns the average
-  irVal1 = 0;
-  irVal2 = 0;
+  leftValue = 0;
+  rightValue = 0;
 
-  // read from the irSensor 5 times
+  // read from the irSensors 5 times
   for (int i = 0; i < 5; i++)
   {
-    irVal1 += analogRead(sensor1);
-    irVal2 += analogRead(sensor2);
+    leftValue += analogRead(leftSensor);
+    rightValue += analogRead(rightSensor);
   }
 
   // divide the value of irValue by 5 to find the average
-  irVal1 = irVal1/5;
-  irVal2 = irVal2/5;
+  leftValue = leftValue/5;
+  rightValue = rightValue/5;
 }
 
 void serialEvent(){
-  // read 1 byte from serial
-  Serial.readBytes(input, 1);
-  ir_threshold = input[0];
+  /*
+   * This function reads bytes from serial at the end of each
+   * loop if there are bytes to read
+   */
+  Serial.readBytes(input, 4); // read 4 bytes from serial
+  mainSpeed = int(input[0]); // The values for the left motor should be between 0 - 255 (size of byte)
+  diffSpeed = int(input[1]); // The values for the right motor should be between 0 - 255
+  irThreshhold = int(input[2]*256+input[3]); // The values for the threshhold can be between 0 - 1023 so there need to be 2 bytes
 }
 
